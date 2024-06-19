cdd79260629a6a0d79f2d70fad9ad635ce570e17
LesnoyPudge <chuykin.a01@gmail.com>
2024-06-19 15:56:27 +0500

    'update'

--- src/hooks/useLatest/useLatest.ts
+++ src/hooks/useLatest/useLatest.ts
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-export const useLatest = <T>(providedValue: T): React.RefObject<T> => {
+export const useLatest = <T>(providedValue: T): React.MutableRefObject<T> => {
     const value = React.useRef(providedValue);
 
     value.current = providedValue;
