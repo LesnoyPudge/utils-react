50cb3b32199bd8936be8590257e6e107da88d4cb
LesnoyPudge <chuykin.a01@gmail.com>
2024-12-09 08:42:49 +0500

    'update'

--- src/hooks/useUpdateEffect/useUpdateEffect.ts
+++ src/hooks/useUpdateEffect/useUpdateEffect.ts
@@ -1,21 +1,19 @@
 import { T } from '@lesnoypudge/types-utils-base/namespace';
 import { useEffect } from 'react';
-import { useFunction } from '@hooks/useFunction';
 import { useIsFirstRender } from '@hooks/useIsFirstRender';
 
 
 
 export const useUpdateEffect = (
-    fn: () => (undefined | (() => void)),
+    fn: () => void,
     deps?: T.UnknownArray,
 ) => {
-    const _fn = useFunction(fn);
     const { getIsFirstRender } = useIsFirstRender();
 
     useEffect(() => {
         if (getIsFirstRender()) return;
 
-        return _fn();
+        return fn();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, deps);
 };
