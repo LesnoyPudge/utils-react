ff3677128110ef529be6580b0feb3662ecaf6e37
LesnoyPudge <chuykin.a01@gmail.com>
2025-02-21 15:37:56 +0500

    'update'

--- vitest.all.ts
+++ vitest.all.ts
@@ -7,19 +7,20 @@ import { nodeConfig } from './vitest.node.config';
 export default defineWorkspace([
     {
         extends: './vite.config.ts',
-        ...mergeConfig(
-            browserConfig,
-            defineConfig({
-                test: {
-                    browser: {
-                        instances: [
-                            { browser: 'firefox' },
-                            { browser: 'webkit' },
-                        ],
-                    },
-                },
-            }),
-        ),
+        ...browserConfig,
+        // ...mergeConfig(
+        //     browserConfig,
+        // defineConfig({
+        //     test: {
+        //         browser: {
+        //             instances: [
+        //                 { browser: 'firefox' },
+        //                 { browser: 'webkit' },
+        //             ],
+        //         },
+        //     },
+        // }),
+        // ),
     },
     {
         extends: './vite.config.ts',
